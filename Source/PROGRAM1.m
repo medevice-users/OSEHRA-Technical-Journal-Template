fb4a1c07fecb92aaa3e4937b41a5ed258f8d1d8b
Luis Ibanez <luis.ibanez@kitware.com>
2011-10-16 14:06:49 -0400

    ENH: First draft with infrastructure.

---
+++ Source/PROGRAM1.m
@@ -0,0 +1,7 @@
+PROGRAM1	;LEI/FO-OAK-ROUTINE TO ILLUSTRATE JOURNAL CONTRIBUTION TO TECHNICAL JOURNAL ;10/16/11  13:50
+	;;7.3;TOOLKIT;**to be determined**;
+	;
+	;  The code in this routine is intended to illustrate how to prepare a
+  ;  contribution to the OSEHRA Technical Journal.  It is expected that
+  ;  all contributions will include source code, and a collection of unit
+  ;  tests that exercise the functionalities of the contributed code.
