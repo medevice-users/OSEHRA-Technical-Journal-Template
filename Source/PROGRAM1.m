4247246d490add1c12f2cce71f5a3349506b30df
Luis Ibanez <luis.ibanez@kitware.com>
2011-10-17 13:09:12 -0400

    ENH: Make a bit  more interesting code example.

--- Source/PROGRAM1.m
+++ Source/PROGRAM1.m
@@ -2,6 +2,10 @@ PROGRAM1	;LEI/FO-OAK-ROUTINE EXAMPLE TO TECHNICAL JOURNAL ;10/16/11  13:50
 	;;7.3;TOOLKIT;**to be determined**;
 	;
 	;  The code in this routine is intended to illustrate how to prepare a
-  ;  contribution to the OSEHRA Technical Journal.  It is expected that
-  ;  all contributions will include source code, and a collection of unit
-  ;  tests that exercise the functionalities of the contributed code.
+	;  contribution to the OSEHRA Technical Journal.  It is expected that
+	;  all contributions will include source code, and a collection of unit
+	;  tests that exercise the functionalities of the contributed code.
+	READ !,"Enter your name",NAME
+	WRITE !,"Welcome to Open Source",NAME
+	QUIT
+	;END OF ROUTINE
